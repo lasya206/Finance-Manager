22fe8a7306a63f3113d6a4ecc6b8e2684cb8d109
azure <github@jamesthomas.anonaddy.com>
2023-08-16 20:02:55 +0530

    test

--- fin_manager/forms.py
+++ fin_manager/forms.py
@@ -5,6 +5,7 @@ class LiabilityForm(forms.ModelForm):
     class Meta:
         model = Liability
         fields = ['name', 'amount', 'interest_rate', 'end_date']
+        
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'amount': forms.NumberInput(attrs={'class': 'form-control'}),
